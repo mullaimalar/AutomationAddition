e88c7a6cb19ba12cbbf60d19652e61f48891cf7a
mullai.malar <mullai.malar@PF-DEV03.internal.private-flight.com>
2017-09-08 15:18:45 +1200

    removed debugging codes

--- src/test/java/com/stepdefinition/AdditionStepDefinition.java
+++ src/test/java/com/stepdefinition/AdditionStepDefinition.java
@@ -14,7 +14,7 @@ import junit.framework.Assert;
 
 public class AdditionStepDefinition {
 public int result;
-HomePage hp= new HomePage(BaseUITest.Driver);
+HomePage homePage= new HomePage(BaseUITest.Driver);
 
 
 @Before
@@ -31,27 +31,34 @@ public void tearDown()
 @Given("^navigate to miniwebtool sum calculator page$")
 public void navigate_to_miniwebtool_sum_calculator_page() throws Throwable {
   
-	 hp.DoLogin();
+	 homePage.DoLogin();
     //BaseUITest.navigateToHomePage();
 }
 
 @When("^the user perform the request to add \"([^\"]*)\" & \"([^\"]*)\"$")
 public void the_user_perform_the_request_to_add(String arg1, String arg2) throws Throwable  {
-		String value = arg1+","+arg2;	
-		SumCalculatorPage calculatorPage = new SumCalculatorPage(BaseUITest.Driver);
+	
+	String value = arg1+","+arg2;	
+		
+	SumCalculatorPage calculatorPage = new SumCalculatorPage(BaseUITest.Driver);
+		
 	calculatorPage.CalculateAddition(value);
+	
 }
 
 @Then("^the sum of \"([^\"]*)\" & \"([^\"]*)\" is \"([^\"]*)\"$")
 public void the_sum_of_is(String arg1, String arg2, String arg3) throws Throwable {
-	SumCalculatorPage calculatorPage = new SumCalculatorPage(BaseUITest.Driver);
- 	Assert.assertEquals(arg3, calculatorPage.GetResult(calculatorPage.TxtValidResult));
 	
+	SumCalculatorPage calculatorPage = new SumCalculatorPage(BaseUITest.Driver);
+	
+ 	Assert.assertEquals(arg3, calculatorPage.GetResult(calculatorPage.TxtValidResult));	
 }
 
 @Then("^the sum of invalid inputs \"([^\"]*)\" & \"([^\"]*)\" is \"([^\"]*)\"$")
 public void the_sum_of_invalid_inputs_is(String arg1, String arg2, String arg3) throws Throwable {
+	
 	SumCalculatorPage calculatorPage = new SumCalculatorPage(BaseUITest.Driver);
+	
  	Assert.assertEquals(arg3, calculatorPage.GetResult(calculatorPage.TxtInvalidResult));
 }
 
